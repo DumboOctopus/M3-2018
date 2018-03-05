e919379c248edc3a81cb9c6b087948e934f00bb1
Neil Prajapati <neilp0101@gmail.com>
2018-03-04 18:57:45 -0800

    1 hour left

--- insert_distances.py
+++ insert_distances.py
@@ -1,6 +1,5 @@
 
-# import necessary packages
-import csv
+#import necessary packages
 import sqlite3
 
 # open the database for storing our data
@@ -11,14 +10,19 @@ c = conn.cursor()
 # this csv was created by copy pasting data from here:
 # http://www.feedingamerica.org/research/map-the-meal-gap/2015/MMG_AllCounties_CDs_MMG_2015_2/TX_AllCounties_CDs_MMG_2015.pdf
 
-with open('county_insecure', 'rb') as csvfile:
-
-    spamreader = csv.reader(csvfile)
-
+with open('Texas_County_Distances.txt') as file:
     # reads each line from the csv
-    for row in spamreader:
-        # inserts the database
-        query = ("INSERT INTO food_insecure_county (county, insecure_population, calories_needed, population) VALUES('{}', {}, {}, {})".format(row[0], row[3], int(row[3]) * 1857, int(row[1])))
+    for line in file:
+
+        # format of lines county_a --> county_b : distance
+        tmp = line.split(" --> ")
+        county_a = tmp[0].replace(" County", "")
+        tmp = tmp[1].split(" : ")
+        county_b = tmp[0].replace(" County", "")
+        # the distance is given as floats but we want integers because its not that percise
+        distance = int(float(tmp[1]))
+
+        query = ("INSERT INTO county_county_distance (county_a, county_b, distance) VALUES('{}', '{}', {})".format(county_a, county_b, distance))
         # print out the query for checking for errors.
         print query
         c.execute(query)
---
+++ insert_food_production.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+# open the database for storing our data
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+
+f = open("W values", "w")
+
+sum_of_counties_nets = 0
+
+with open("food_production_county.tsv") as file:
+    for line in file:
+        splitting = [i.replace(",", "") for i in line.split("\t")]
+        county = splitting[0]
+        # County cattle wheat milk_cows corn sorghum oats rice soybean	sunflower_oil	sugarcane	peanuts	sunflower_nonoil
+        cattle = 0.095 * 974 * int(splitting[1])
+        wheat = 0.14 * 179 * int(splitting[2])
+        milk_cows = 0.052 * 13824 * int(splitting[3])
+        corn = 0.14 *268 * int(splitting[4])
+        sorghum = 0.14 *236 * int(splitting[5])
+        oats = 0.14 *155 * int(splitting[6])
+        rice = 0.14 *138 * int(splitting[7])
+        soybean = 0.14 * 333 * int(splitting[8])
+        sunflower_oil = 0.17 * int(splitting[9])
+        sugarcan = .26 * 648 * int(splitting[10])
+        peanut = 0.17* 7.05 * int(splitting[11])
+        sunflower_nonoil = 0.17 * int(splitting[12])
+
+
+        # total calories we can gain from not wasting this stuff at production
+        calories = cattle + wheat + milk_cows + corn + sorghum + oats + rice + soybean + sunflower_oil + sugarcan + peanut + sunflower_nonoil;
+        groceries_in_this_county = int(c.execute("SELECT population from food_insecure_county where county= '{}'".format(county)).fetchone()[0])/7537
+        calories_wasted_by_retail = 0.0378 * groceries_in_this_county * 2231
+        calories += calories_wasted_by_retail
+
+        net = int(calories) - int(c.execute("SELECT calories_needed from food_insecure_county where county= '{}'".format(county)).fetchone()[0])
+
+        query = "INSERT INTO county_net_food (county, net) VALUES('{}', {})".format(county, net)
+        print query
+        c.execute(query)
+
+        f.write(county + "   " + str(calories))
+        f.write("\n")
+f.write("net sum " + str(sum_of_counties_nets))
+
+
+# remove this line so it doesn't actually add more stuff to db.
+#conn.commit()
+conn.close()
